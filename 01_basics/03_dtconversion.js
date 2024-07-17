c352cde92a9ad2362fe63af98b4fb6072ca21710
sowdhamini17 <73640839+sowdhamini17@users.noreply.github.com>
2024-07-17 14:02:44 +0000

    dtcov

---
+++ 01_basics/03_dtconversion.js
@@ -0,0 +1,14 @@
+let value = 33;
+let value1 = null;
+let value2 = undefined;
+let num =Number(value2);
+console.log(typeof num);
+console.log(num);
+
+let st = String(value);
+console.log(typeof st);
+
+let bool=Boolean(value);
+let bool1=0;
+console.log(bool1);
+console.log(typeof bool1);
